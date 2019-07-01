25fc3657c11c4f0a023499ead9929d35f889ef26
Theis Frøhlich <theisof@gmail.com>
2019-07-01 10:11:28 +0200

    dist path changed

--- index.js
+++ index.js
@@ -7,11 +7,6 @@ app
   .options('*', cors())
   .listen(3500, '0.0.0.0')
 
-app.get('/', function(req,res) {
-  res.sendfile(__dirname + '/index.html');
-})
-
-
 app.use('/', express.static(__dirname + '/dist'))
 
 console.log('App served on http://localhost:3500')
