9aa4a72b32a76b39dd641aa5cf302ed84e3cb79d
Theis Frøhlich <theisof@gmail.com>
2019-07-04 13:11:05 +0200

    navigation and logos

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 const gulp = require('gulp')
 const { watch } = require('gulp')
+const rename = require('gulp-rename')
 const prefix = require('gulp-autoprefixer')
 let cleanCSS = require('gulp-clean-css');
 let sass = require('gulp-sass')
@@ -19,7 +20,26 @@ function copyFonts() {
     .pipe(gulp.dest('./dist/fonts/'))
 }
 
+function copyLogos() {
+  return gulp.src(['./src/logos/**/*'])
+    .pipe(rename((path) => {
+      const cleanName = (name) => {
+        return name
+          .replace(/\s/g, '-')
+          .replace(/\'/g, '-')
+          .replace(/\_/g, '-')
+          .toLowerCase()
+      }
+
+      path.dirname = cleanName(path.dirname)
+      path.basename = cleanName(path.basename)
+
+    }))
+    .pipe(gulp.dest('./dist/'))
+}
+
 exports.default = () => {
   watch('./src/sass/**/*.scss', css);
   watch('./src/fonts/**/*', copyFonts);
+  watch('./src/logos/**/*', copyLogos);
 };
